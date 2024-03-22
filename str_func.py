07e37d9dd3a0747d34fcae98d2fade8776037292
Artem Brazhnik <dambldor55@gmail.com>
2024-03-22 22:44:20 +0300

    fix str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_upper(string):
+    """"возвращает текст заглавными"""
     print(string.upper())
+
+
+def every_title(snring):
+    """каждая первая - заглавная"""
+    print(string.title())
